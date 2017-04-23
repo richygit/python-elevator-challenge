fb652733a9e8d42282046da9a895ad5debfef78b
rich <rich@richs-MacBook-Pro.local>
2017-04-23 21:14:09 +0900

    added logic to ensure elevator stops at last request in the current direction

--- elevator.py
+++ elevator.py
@@ -29,6 +29,7 @@ class ElevatorLogic(object):
         down = [None] * size
         self.called = [None, up, down]
         self.selected = [None] * size
+        #set when motor not moving to indicate the service direction
         self.movement_direction = None
 
     def on_called(self, floor, direction):
@@ -42,10 +43,6 @@ class ElevatorLogic(object):
         """
         eprint('## Floor called: ' , floor , ',direction: ' , direction , ', current: ' , self.current_floor())
         self.record_call(floor, direction)
-
-        # import pdb
-        #pdb.set_trace()
-
         self.move_check()
 
     def on_floor_selected(self, floor):
@@ -78,6 +75,14 @@ class ElevatorLogic(object):
 
         ########################
 
+    def not_moving(self):
+        return self.movement_direction == None and self.callbacks.motor_direction == None
+
+    def opposite(self, direction):
+        if direction == UP:
+            return DOWN
+        else:
+            return UP
 
     def relative_direction(self, floor):
         if self.current_floor() < floor:
@@ -93,18 +98,57 @@ class ElevatorLogic(object):
     def is_current_call_in_same_direction(self, direction):
         return self.called[direction][self.current_floor()] and self.callbacks.motor_direction == direction
 
+    def is_current_call_in_opposite_direction(self):
+        if self.callbacks.motor_direction == UP:
+            return self.called[DOWN][self.current_floor()] 
+        else:
+            return self.called[UP][self.current_floor()] 
+
+    def clear_current_call_in_opposite_direction(self):
+        if self.callbacks.motor_direction == UP:
+            self.called[DOWN][self.current_floor()] = None
+        else:
+            self.called[UP][self.current_floor()] = None
+
+    def more_requests_in_current_direction(self):
+        return self.more_requests_in_direction(self.callbacks.motor_direction)
+
+    def more_requests_in_direction(self, direction):
+        if direction == UP:
+            return self.list_idx(self.called[UP][self.current_floor():]) or \
+                self.list_idx(self.called[DOWN][self.current_floor():]) or \
+                self.list_idx(self.selected[self.current_floor():])
+        elif self.callbacks.motor_direction == DOWN:
+            return self.list_idx(self.called[UP][:self.current_floor()]) or \
+                self.list_idx(self.called[DOWN][:self.current_floor()]) or \
+                self.list_idx(self.selected[:self.current_floor()])
+
+    def final_request_in_current_direction(self):
+        if self.not_moving():
+            return False
+        else:
+            return not self.more_requests_in_current_direction()
+
+
     def should_stop(self):
-        return self.is_current_call_in_same_direction(UP) or self.is_current_call_in_same_direction(DOWN) or self.selected[self.current_floor()]
+        return self.is_current_call_in_same_direction(UP) or \
+            self.is_current_call_in_same_direction(DOWN) or \
+            self.final_request_in_current_direction() or \
+            self.selected[self.current_floor()]
 
     """ returns the index of the first True elem in the 'called' array for the given direction
     """
     def called_idx(self, direction):
-        for i, j in enumerate(self.called[direction]):
+        return self.list_idx(self.called[direction])
+
+    def list_idx(self, lst):
+        for i, j in enumerate(lst):
             if j:
                 return i
         return None
 
-    def next_call_direction(self):
+    def next_request_direction(self):
+        #TODO this only services calls, not selections
         up = self.called_idx(UP)
         if up:
             return self.relative_direction(up)
@@ -112,20 +156,36 @@ class ElevatorLogic(object):
         if down:
             return self.relative_direction(down)
 
+    """ returns the movement direction """
     def clear_current_request(self):
-        if self.selected[self.current_floor]:
-            self.selected[self.current_floor] = None
+        if self.selected[self.current_floor()]:
+            self.selected[self.current_floor()] = None
+            if self.more_requests_in_current_direction():
+                return self.callbacks.motor_direction
+            else:
+                return self.next_request_direction()
         elif self.is_current_call_in_same_direction(DOWN):
             self.called[DOWN][self.current_floor()] = None
+            return self.callbacks.motor_direction
         elif self.is_current_call_in_same_direction(UP):
             self.called[UP][self.current_floor()] = None
+            return self.callbacks.motor_direction
+        elif not self.more_requests_in_current_direction():
+            if self.is_current_call_in_opposite_direction():
+                self.clear_current_call_in_opposite_direction()
+                return self.opposite(self.callbacks.motor_direction)
+            elif self.more_requests_in_direction(self.opposite(self.callbacks.motor_direction)):
+                return self.opposite(self.callbacks.motor_direction)
+                
+
 
     def direction_to_move(self):
+
         if self.should_stop():
-            self.clear_current_request()
+            self.movement_direction = self.clear_current_request()
             return None
         else:
-            return self.next_call_direction()
+            return self.next_request_direction()
 
     def record_call(self, floor, direction):
         if(self.callbacks.current_floor == floor):
@@ -133,9 +193,7 @@ class ElevatorLogic(object):
         self.called[direction][floor] = True
 
     def set_motor_direction(self,direction):
-        if direction == None:
-            self.movement_direction = self.callbacks.motor_direction
-        else:
+        if direction != None:
             self.movement_direction = None
         eprint('## motor set: ', direction)
         self.callbacks.motor_direction = direction
